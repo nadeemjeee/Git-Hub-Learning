cc3684fdc0edda35bdc772be278208337089aba3
Nadeem Ahmad <nadeemjeee@gmail.com>
2026-03-24 18:49:06 +0100

    Initial Code

---
+++ counting.java
@@ -0,0 +1,9 @@
+public class counting {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 20;
+        int sum = num1+num2;
+
+        System.out.println("Total is : " + sum);
+    }
+}
