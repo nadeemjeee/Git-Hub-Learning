ac983009f37ba0d5402559c4d3de93b031365853
Nadeem Ahmad <nadeemjeee@gmail.com>
2026-03-25 18:00:40 +0100

    Added new file(School.java)

---
+++ school.java
@@ -0,0 +1,10 @@
+public class school {
+
+    public static void main(String[] args) {
+        String name = "Moosa";
+        int age = 15;
+        String school = "Pauli, Malmö";
+
+        System.err.println(name + " is "+age + " years old"+ " and going " + school);
+    }
+}
